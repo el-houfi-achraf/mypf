40f2664d3cfc5453b450458061c4d33b815d80f9
ACHRAF  EL HOUFI <130187102+el-houfi-achraf@users.noreply.github.com>
2024-12-24 14:20:32 +0100

    -sk

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import CustomCursor from "./components/CustomCursor";
@@ -61,7 +60,6 @@ function App() {
             <Hero />
             <About />
             <Projects />
-            <Skills />
             <Contact showNotification={showNotification} />
           </main>
           <Footer />
